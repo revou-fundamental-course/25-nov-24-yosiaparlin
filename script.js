3bcead976cb4a0b421cf3043bbc580b4101fe3c9
yosiaparlin <yosiaparlins@gmail.com>
2025-01-31 15:49:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
-//Ini File Javascript
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
+let resultArea = document.querySelector(".comment");
 
-const bmiText = document.getElementById("bmi");
-const descText = document.getElementById("desc");
-const form = document.querySelector("form");
+modalContent = document.querySelector(".modal-content");
+modalText = document.querySelector("#modalText");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
 
-form.addEventListener("submit", onFormSubmit)
-form.addEventListener("reset", onFormReset);
 
-function onFormReset() {
-  bmiText.textContent = 0;
-  bmiText.className = "";
-  descText.textContent = "N/A";  
+function calculate(){
+ 
+  if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+    modal.style.display = "block";
+    modalText.innerHTML = `All fields are required!`;
+
+  }else{
+    countBmi();
+  }
+
 }
 
-function onFormSubmit(e) {
-    e.preventDefault();
 
-    const weight = parseFloat(form.weight.value);
-    const height = parseFloat(form.height.value);
+function countBmi(){
+  var p = [age.value, height.value, weight.value];
+  if(male.checked){
+    p.push("male");
+  }else if(female.checked){
+    p.push("female");
+  }
 
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-        alert ("Masukkan data yang valid");
-        return;
-    }
-    const heightInMeters = height / 100; //cm -> m
-    const bmi = weight / Math.pow(heightInMeters, 2);
-    const desc = interpretBMI (bmi);
+  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+      
+  var result = '';
+  if(bmi<18.5){
+    result = 'Underweight';
+     }else if(18.5<=bmi&&bmi<=24.9){
+    result = 'Healthy';
+     }else if(25<=bmi&&bmi<=29.9){
+    result = 'Overweight';
+     }else if(30<=bmi&&bmi<=34.9){
+    result = 'Obese';
+     }else if(35<=bmi){
+    result = 'Extremely obese';
+     }
 
-    bmiText.textContent = bmi.toFixed(2);
-    bmiText.className = desc;
-    descText.innerHTML = `Berat Badan Anda <strong>${desc}</strong>`;
+
+
+resultArea.style.display = "block";
+document.querySelector(".comment").innerHTML = `You are <span id="comment">${result}</span>`;
+document.querySelector("#result").innerHTML = bmi.toFixed(2);
+
 }
 
-function interpretBMI(bmi) {
-    if (bmi < 18.5) {
-        return "kurang";
-    } else if (bmi < 25) {
-        return "ideal";
-    } else if (bmi < 30) {
-        return "berlebih";
-    } else {
-        return "obesitas";
-    }
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
